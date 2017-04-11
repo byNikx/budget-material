1d0b8c09eb50b12c7b1c71300c998a1480d77f3a
byNikx <w3c.nadeem@hotmail.com>
2017-04-11 18:46:32 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,43 @@ export class AppComponent {
     {
       name: "Income",
       actual: 34543,
-      budget: 5656
+      budget: 5656,
+      notification: false
     },{
       name: "Car Transportation",
       actual: 37856,
-      budget: 784354
+      budget: 784354,
+      notification: false
     },{
       name: "Children",
       actual: 29989,
-      budget: 876212
+      budget: 876212,
+      notification: false
     },{
       name: "Education",
       actual: 44543,
-      budget: 67855
+      budget: 67855,
+      notification: true
     },{
       name: "Entertainment & Hobbies",
       actual: 78944,
-      budget: 876212
+      budget: 876212,
+      notification: false
     },{
       name: "Fines, Late Fees, Loans",
       actual: 67533,
-      budget: 66895
+      budget: 66895,
+      notification: false
     },{
       name: "Bank Fees & Service Fees",
       actual: 23643,
-      budget: 875266
+      budget: 875266,
+      notification: false
     },{
       name: "Consumption Loan Payment",
       actual: 124433,
-      budget: 656445
+      budget: 656445,
+      notification: false
     },
   ];
 }
